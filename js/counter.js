a009d9367389d6f6a31ae6ba5f7a7e53cd657bee
Moin Khan <kingkhan@engineer.com>
2017-10-04 23:15:25 +0530

    counter added

---
+++ js/counter.js
@@ -0,0 +1,12 @@
+ /*
+Counter for Website
+
+(c) Moin Khan, Alpha Magnus Studios
+  https://www.alphamstudios.com
+*/
+
+
+ function Counter()
+{
+  document.write('<img src="http://simplehitcounter.com/hit.php?uid=2305301&f=0&b=16777215" border="0" height="18" width="83" alt="AMS_web_counter">');
+}
