953b1d3f53d1513737fef31a64b975e3440239d8
Moin Khan <kingkhan@engineer.com>
2017-11-05 00:29:37 +0530

    Fixed: removed back links from all files

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -1,16 +1,7 @@
-/*!
- * jQuery JavaScript Library v3.2.1
- * https://jquery.com/
- *
- * Includes Sizzle.js
- * https://sizzlejs.com/
- *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2017-03-20T18:59Z
+/*
+ * (c) Alpha Magnus Studios
  */
+ 
 ( function( global, factory ) {
 
 	"use strict";
